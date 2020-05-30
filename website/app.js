7afb451718c2b1550ab467d333876dd1bb51f476
Jihad tarek <jihadtarek60@gmail.com>
2020-05-30 02:13:51 +0200

    update baseURL

--- website/app.js
+++ website/app.js
@@ -5,7 +5,7 @@ let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
-const baseURL ='https://openweathermap.org/data/2.5/weather?zip=';
+const baseURL ='https://api.openweathermap.org/data/2.5/weather?zip=';
 const APIkey = 'e5f1d30dabc61588114bc27d77162b8e';
 
 // Event listener to add function to existing HTML DOM element
@@ -19,46 +19,29 @@ function performAction(e){
     getData(baseURL,zip,APIkey)
     .then(function(temperature){
         //add data
-        console.log(data);
-        postData('/add',{temp:temperature,date:newDate,content:newFeelings});
-        updateUI();
-    });
-    
+        console.log(temperature);
+        postData('/add',{temp:temperature,date:newDate,content:newFeelings})
+        updateUI()
+
+    })    
 }
 
 /* Function to GET Web API Data*/
 const getData = async (baseURL,zip,key)=>{
-    const res =await fetch(baseURL + zip + ',eg&APPID=' + key)
-   // const res =await fetch('/fakedata')
-
+    const res =await fetch(baseURL + zip + ',us&APPID=' + key)
     try{
         const data = await res.json();
         const temperature=data.main.temp;
-        //console.log(data)
         return temperature;
     }catch(error){
         console.log("error",error);
     }
-}
-
-/* Function to GET Project Data */
-
-const getProjectData = async(url='')=>{
-    const res = await fetch(url);
-    try{
-        const allData =await res.json();
-        return allData;
-    }catch(error){
-        console.log("error",error);
-    }
-
 }
 /* Function to POST data */
 const postData = async(url='',data={})=>{
 
     const response = await fetch(url,{
         method:'POST',
-       // mode:'no-cors',
         credentials:'same-origin',
         headers:{
             'content-type':'application/json',
@@ -79,17 +62,32 @@ const postData = async(url='',data={})=>{
 
 }
 
+/* Function to GET Project Data */
+
+const getProjectData = async(url='')=>{
+    const res = await fetch(url);
+    try{
+        const allData =await res.json();
+        return allData;
+    }catch(error){
+        console.log("error",error);
+    }
+
+}
 
 /*UPDATEUI */
 const updateUI = async()=>{
 
     const request = await fetch('/all');
     try{
-        const allData = await request.json();
+        let allData = await request.json();
+        allData = allData.data
         console.log(allData);
-        document.getElementById('date').innerHTML=allData[0].date;
-        document.getElementById('temp').innerHTML=allData[0].temperature;
-        document.getElementById('content').innerHTML=allData[0].content;
+        for(let i=0;i<allData.length;i++){
+        document.getElementById('date').innerHTML=allData[i].date;
+        document.getElementById('temp').innerHTML=allData[i].temperature;
+        document.getElementById('content').innerHTML=allData[i].content;
+        }
 
     }catch(error){
         console.log("error",error);
